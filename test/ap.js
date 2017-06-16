35105e8fa21277bdc9e5303a60a727eb88c3a08f
Anticrisis <anticrisisg@gmail.com>
2017-06-15 14:25:56 -1000

    ap test

--- test/ap.js
+++ test/ap.js
@@ -1,6 +1,6 @@
 var R = require('..');
 var eq = require('./shared/eq');
-
+var I = require('immutable');
 
 describe('ap', function() {
   function mult2(x) { return x * 2; }
@@ -34,6 +34,43 @@ describe('ap', function() {
     var val = R.ap([mult2, plus3]);
     eq(typeof val, 'function');
     eq(val([1, 2, 3]), [2, 4, 6, 4, 5, 6]);
+  });
+
+});
+
+
+describe('Immutable: ap', function() {
+  function mult2(x) { return x * 2; }
+  function plus3(x) { return x + 3; }
+
+  it('interprets [a] as an applicative', function() {
+    eq(R.ap([mult2, plus3], I.List([1, 2, 3])), I.List([2, 4, 6, 4, 5, 6]));
+  });
+
+  it('interprets ((->) r) as an applicative', function() {
+    var f = function(r) {
+      return function(a) {
+        return r + a;
+      };
+    };
+    var g = function(r) { return r * 2; };
+    var h = R.ap(f, g);
+    // (<*>) :: (r -> a -> b) -> (r -> a) -> (r -> b)
+    // f <*> g = \x -> f x (g x)
+    eq(h(10), 10 + (10 * 2));
+
+    eq(R.ap(R.add)(g)(10), 10 + (10 * 2));
+  });
+
+  it('dispatches to the passed object\'s ap method when values is a non-Array object', function() {
+    var obj = {ap: function(n) { return 'called ap with ' + n; }};
+    eq(R.ap(obj, 10), obj.ap(10));
+  });
+
+  it('is curried', function() {
+    var val = R.ap([mult2, plus3]);
+    eq(typeof val, 'function');
+    eq(val(I.List([1, 2, 3])), I.List([2, 4, 6, 4, 5, 6]));
   });
 
 });
